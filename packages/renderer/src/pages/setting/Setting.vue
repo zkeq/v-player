f25a03dfaba4474e3a3559aaa6525dbb65a57bd3
gumengyu <1415515984yuri@gmail.com>
2022-08-24 17:58:04 +0800

    feat: add some tips

--- packages/renderer/src/pages/setting/Setting.vue
+++ packages/renderer/src/pages/setting/Setting.vue
@@ -4,6 +4,9 @@
       <h6 class="text-h5 mb-4">{{ $t('common.setting') }}</h6>
       <app-settings-theme />
       <app-settings-other />
+      <v-alert border="start" variant="tonal" closable close-label="小贴士" color="primary" title="小贴士">
+        应用无响应时，通过ctrl + r / cmd + r 可以刷新重启应用，可以解决一部分问题。
+      </v-alert>
     </div>
   </section>
 </template>
